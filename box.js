2e025f3d2d43d81d189a25f4df67d408b5e416d2
Neermay10 <74848380+Neermay10@users.noreply.github.com>
2020-12-02 16:49:55 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,23 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            istatic:true,
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        //this.width = Box.width;
+        //this.height = Box.height;
+        fill("red");
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+
+
+    }
+}
